7b2aa66d0c9de322b76a80005c0c7a8b9c47c067
sheverda <nsewerda04@gmail.com>
2024-03-17 00:32:39 +0200

    final verion

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const menu_burger = document.querySelector(".menu_burger")
 const nav_menu_burger = document.querySelector(".nav_menu_burger")
 const menu_burger_items = document.querySelectorAll(".menu_burger_item")
+const body = document.querySelector("body")
+
+
+window.addEventListener('resize', function() {
+    let newScreenWidth = window.innerWidth;
+    if (newScreenWidth > 850) {
+        nav_menu_burger.classList.remove("active")
+        body.style.overflow = "visible"
+        menu_burger.classList.remove("active")
+    }
+});
+
 
 menu_burger.addEventListener("click", ()=> {
     menu_burger.classList.toggle("active")
@@ -8,11 +20,13 @@ menu_burger.addEventListener("click", ()=> {
         nav_menu_burger.classList.add('active')
         menu_burger_items.forEach(item => {
             item.classList.add('active')
+            body.style.overflow = "hidden"
         })
     } else {
         nav_menu_burger.classList.remove('active')
         menu_burger_items.forEach(item => {
             item.classList.remove("active")
+            body.style.overflow = "visible"
         })
     }
 })
